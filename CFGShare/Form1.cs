ea15e01b8d187d11ca9b8efdabefb8e1828582b2
gamingdoom <pranaysanghai@gmail.com>
2020-08-03 19:14:10 -0700

    Update Form1.cs

--- CFGShare/Form1.cs
+++ CFGShare/Form1.cs
@@ -87,7 +87,7 @@ namespace CFGShare
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //e
         }
 
         private void button5_Click(object sender, EventArgs e)
